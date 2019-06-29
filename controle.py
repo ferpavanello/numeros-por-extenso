3085c82a2b292ae364d1334a4083177504d06032
Fernando <fernando.pavanello2@gmail.com>
2019-06-29 22:47:22 +0000

    Tratando números negativos

--- controle.py
+++ controle.py
@@ -7,13 +7,16 @@ def controle(req):
     tamanho = len(req)
     retorno = ""
 
-    if tamanho == 1:
-        retorno = pega_unidade(req)
-    elif tamanho == 2:
-        retorno = pega_dezena(req)
-    elif tamanho == 3:
-        retorno = pega_centena(req)
-    else:
-        retorno = pega_milhar(req)
+    if req[0] == "-":
+        retorno += "menos "
 
-    print(escrito)
+    if tamanho == 1:
+        retorno += pega_unidade(req)
+    elif tamanho == 2:
+        retorno += pega_dezena(req)
+    elif tamanho == 3:
+        retorno += pega_centena(req)
+    else:
+        retorno += pega_milhar(req)
+
+    print(retorno)
