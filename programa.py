0debece92ba4460e321a38a40ff5039ad6b3b480
Fernando <fernando.pavanello2@gmail.com>
2019-06-29 17:57:53 +0000

    Git init

---
+++ programa.py
@@ -0,0 +1,31 @@
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+
+
+class S(BaseHTTPRequestHandler):
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_GET(self):
+        url = self.path
+        print(url)
+        self._set_headers()
+        self.wfile.write("<html><head><title>Teste</title></head>")
+        self.wfile.write("<body><p>This is a test.</p>")
+
+
+def run(server_class=HTTPServer, handler_class=S, port=7000):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print('Starting httpd...')
+    httpd.serve_forever()
+
+
+if __name__ == "__main__":
+    from sys import argv
+
+    if len(argv) == 2:
+        run(port=int(argv[1]))
+    else:
+        run()
